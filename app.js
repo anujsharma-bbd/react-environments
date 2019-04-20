47546c7ff1b269302c99c534bba45eeac9c01542
cc311421 <anuj.sharma01@nagarro.com>
2019-04-21 01:36:01 +0530

    fixed issue while implementing in real time projects

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 let path = require('path');
 let fs = require('fs');
-const envfile = path.join(__dirname, '/environments/environment.js');
-const destinationFile = require(envfile);
 
 const args = require('minimist')(process.argv.slice(2));
-console.log("react-env args:", args);
-if (!args || !args.env) {
-   throw new Error("Enviroment variable not passed. pass liek --env = ABC");
+
+let env = (args.env || process.env.npm_config_env), envPath = (args.path || "src/environments");
+
+if (!args || !env) {
+   throw new Error("Enviroment variable not passed. pass like --env = ABC");
 }
-let env = args.env;
-console.log("react-env env:", env);
-const desiredEnvFile = path.join(__dirname, '/environments/environment.' + env + '.js');
+
+const envfile = path.join(__dirname, `../../${envPath}/environment.js`);
+
+const destinationFile = require(envfile);
+const desiredEnvFile = path.join(__dirname, `../../${envPath}/environment.${env}.js`);
+
 let sourceFile = require(desiredEnvFile);
+
 let allProps = Object.assign({}, destinationFile, sourceFile);
+
 allProps = "module.exports = " + JSON.stringify(allProps);
 
 fs.writeFile(envfile, allProps, (err) => {
