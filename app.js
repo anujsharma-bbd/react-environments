42f4369b74b1d3cb1832aaa66c7e1fb2a2ceb962
cc311421 <anuj.sharma01@nagarro.com>
2019-04-21 01:53:05 +0530

    added environments file merging logic

--- app.js
+++ app.js
@@ -3,24 +3,23 @@ let fs = require('fs');
 
 const args = require('minimist')(process.argv.slice(2));
 
-let env = (args.env || process.env.npm_config_env), envPath = (args.path || "src/environments");
+let env = (args.env || process.env.npm_config_env || 'dev'), envPath = (args.path || 'src/environments');
 
 if (!args || !env) {
-   throw new Error("Enviroment variable not passed. pass like --env = ABC");
+   throw new Error('Enviroment variable not passed. pass like --env = ABC');
 }
 
 const envfile = path.join(__dirname, `../../${envPath}/environment.js`);
-
-const destinationFile = require(envfile);
 const desiredEnvFile = path.join(__dirname, `../../${envPath}/environment.${env}.js`);
 
-let sourceFile = require(desiredEnvFile);
+const destinationFile = require(envfile);
+const sourceFile = require(desiredEnvFile);
 
 let allProps = Object.assign({}, destinationFile, sourceFile);
 
-allProps = "module.exports = " + JSON.stringify(allProps);
+allProps = 'module.exports = ' + JSON.stringify(allProps);
 
 fs.writeFile(envfile, allProps, (err) => {
    if (err) throw err;
-   console.log('Saved!');
+   console.log(`${env} environment file Saved!`);
 })
