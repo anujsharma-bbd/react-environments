6ac816db4976362521746f3a18863ba18c3aeb92
Anuj Sharma <sharmaanuj@hotmail.com>
2020-03-20 03:15:53 +0530

    trying watch on env file change

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
+let watch = require('node-watch');
 let path = require('path');
 let fs = require('fs');
-
 const args = require('minimist')(process.argv.slice(2));
 
-let env = (args.env || process.env.npm_config_env || 'dev'), envPath = (args.path || 'src/environments');
 
+
+let env = (args.env || process.env.npm_config_env || 'dev'), envPath = (args.path || 'src/environments');
+let isBuild = (args.build || process.env.npm_config_build || false);
 if (!args || !env) {
    throw new Error('Enviroment variable not passed. pass like --env = ABC');
 }
@@ -12,14 +14,28 @@ if (!args || !env) {
 const envfile = path.join(__dirname, `../../${envPath}/environment.js`);
 const desiredEnvFile = path.join(__dirname, `../../${envPath}/environment.${env}.js`);
 
-const destinationFile = require(envfile);
-const sourceFile = require(desiredEnvFile);
+if (!isBuild) {
+   let watcher = watch([desiredEnvFile], { persistent: true, recursive: false },
+      (ev, name) => {
+      });
 
-let allProps = Object.assign({}, destinationFile, sourceFile);
+   watcher.on('change', (evt, name) => {
+      changeFile();
+   });
+   process.on('SIGINT', watcher.close);
+}
 
-allProps = 'module.exports = ' + JSON.stringify(allProps, null, 4);
+const changeFile = () => {
 
-fs.writeFile(envfile, allProps, (err) => {
-   if (err) throw err;
-   console.log(`${env} environment file Saved!`);
-})
+   const destinationFile = require(envfile);
+   const sourceFile = require(desiredEnvFile);
+   let allProps = Object.assign({}, destinationFile, sourceFile);
+
+   allProps = 'module.exports = ' + JSON.stringify(allProps, null, 4);
+
+   fs.writeFile(envfile, allProps, (err) => {
+      if (err) throw err;
+      console.log(`${env} environment file Saved!`);
+   })
+}
+changeFile();
